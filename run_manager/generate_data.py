0d235907dd2fab377051db07b36d0d4c56b23f99
Frederik Wilde <fwilde@physik.fu-berlin.de>
2024-10-29 16:57:46 +0100

    Add DataSet dataclass for proper loading and saving

--- run_manager/generate_data.py
+++ run_manager/generate_data.py
@@ -1,7 +1,9 @@
-import sys
+import os
+from typing import List
 import jax.numpy as jnp
 import jax
 from time import time
+from dataclasses import dataclass
 from datetime import datetime, UTC
 import h5py
 from pathlib import Path
@@ -110,57 +112,194 @@ def ini_mps(num_sites, chi, mps_perturbation, local_dim, occupation, rng=None):
     return m
 
 
-def compute_samples(name, cf, steps, seed, num_samples, ini_state_occupation):
+@dataclass
+class DataSet:
+    '''Defines the schema for datasets and provides loading and saving methods `to_hdf5` and `from_hdf5`.'''
+    # meta data
+    num_sites: int
+    true_parameters: jnp.ndarray  # J, U, *mu
+    ini_state: str
+    times: List[float]
+    # data
+    samples_list: List[jnp.ndarray]
+    mps_list: List[jnp.ndarray] | None
+    errors_squared_list: List[jnp.ndarray]
+    # simulation meta data
+    chi: int
+    deltat: int
+    local_dim: int
+    prng_seed: int
+    run_manager_commit_hash: str
+    differentiable_tebd_commit_hash: str
+    debug_mode: bool
+
+    def to_hdf5(self, filepath: str | Path):
+        with h5py.File(filepath, 'x') as f:
+            g_samples = f.create_group('samples')
+            g_mps = f.create_group('mps')
+            g_errs = f.create_group('errors_squared')
+
+            assert len(self.times) == len(self.samples_list)
+            if self.mps_list is None:
+                raise ValueError('MPS list not set. This is likely, because the dataset was loaded from another file.')
+            assert len(self.times) == len(self.mps_list)
+            assert len(self.times) == len(self.errors_squared_list)
+
+            for i, (s, m, e) in enumerate(zip(self.samples_list, self.mps_list, self.errors_squared_list)):
+                g_samples.create_dataset(f't{i}', data=s)
+                g_mps.create_dataset(f't{i}', data=m)
+                g_errs.create_dataset(f't{i}', data=e)
+
+            for k, v in self.__dict__.items():
+                if not k in ['samples_list', 'mps_list', 'errors_squared_list']:
+                    f.attrs[k] = v
+
+    @classmethod
+    def from_hdf5(
+        cls,
+        filepath: str | Path,
+        *,
+        time_stamp_selection: List[int] | None = None,
+        load_mps=False,
+        num_samples=None
+    ):
+        with h5py.File(filepath, 'r') as f:
+            if time_stamp_selection is None:
+                time_stamp_selection = list(range(len(f.attrs['times'])))
+
+            if num_samples is None:
+                num_samples = f['samples/t0'].shape[0]
+
+            if load_mps:
+                mps_list = [f[f'mps/t{i}'][()] for i in time_stamp_selection]
+            else:
+                mps_list = None
+
+            samples_list = [f[f'samples/t{i}'][:num_samples] for i in time_stamp_selection]
+            errors_squared_list = [f[f'errors_squared/t{i}'][()] for i in time_stamp_selection]
+
+            meta_data_keys = [
+                'num_sites',
+                'true_parameters',
+                'ini_state',
+                'chi',
+                'deltat',
+                'local_dim',
+                'prng_seed',
+                'run_manager_commit_hash',
+                'differentiable_tebd_commit_hash',
+                'debug_mode'
+            ]
+            kwargs = {k: f.attrs[k] for k in meta_data_keys}
+
+            dataset = cls(
+                times=[f.attrs['times'][i] for i in time_stamp_selection],
+                samples_list=samples_list,
+                mps_list=mps_list,
+                errors_squared_list=errors_squared_list,
+                **kwargs
+            )
+
+        return dataset
+
+
+def compute_samples(
+        name,
+        num_sites,
+        chi,
+        deltat,
+        true_parameters,
+        local_dim,
+        steps,
+        seed,
+        num_samples,
+        ini_state,
+        *,
+        dataset_containing_mps: str | None = None
+    ):
     '''Generate samples.
 
     Args:
         name (str): File name of the data set.
-        cf (object): SimpleNamespace object which has the following attributes.
-            num_sites, chi, deltat, J, U, mu, local_dim
+        num_sites (int)
+        chi (int)
+        deltat (int)
+        true_parameters (jax.numpy.ndarray): Consisting of J, U, *mu
+        local_dim (int)
         steps (Sequence[int]): Number of Trotter steps to get to the _next_ time stamp.
             E.g. [10, 10, 10] would generate data after 10, 20, and 30 Trotter steps.
         seed (int): PRNG seed.
         num_samples (int): How many samples to draw per time step.
-        ini_state_occupation (str): Gets passed on to ini_mps.
+        ini_state (str): Gets passed on to ini_mps.
+
+    Kwargs:
+        dataset_containing_mps (str | None): If set, it must refer to a valid DataSet HDF5 file.
+            Uses the MPS from the dataset instead of simulating the time evolution.
     '''
     key = jax.random.PRNGKey(seed)
     keys = jax.random.split(key, (len(steps), num_samples))
 
-    cf.run_manager_commit_hash = COMMIT_HASH
     DIFFERENTIABLE_TEBD_DIR = load_dir_var('DIFFERENTIABLE_TEBD_DIR')
-    cf.differentiable_tebd_hash = get_commit_hash(DIFFERENTIABLE_TEBD_DIR)
 
-    params = jnp.concatenate([jnp.array([cf.J, cf.U]), cf.mu])
+    m = ini_mps(num_sites, chi, None, local_dim, ini_state)
+    times = []
+    samples_list = []
+    mps_list = []
+    errors_squared_list = []
 
-    m = ini_mps(cf.num_sites, cf.chi, None, cf.local_dim, ini_state_occupation)
-    cf.times = []
+    if dataset_containing_mps is not None:
+        dataset = DataSet.from_hdf5(dataset_containing_mps, num_samples=1, load_mps=True)
+        mps_iterator = iter(dataset.mps_list)
+        errors_iterator = iter(dataset.errors_squared_list)
+
+        # Some checks to ensure that the the dataset matches the parameters
+        # specified in the arguments.
+        assert num_sites == dataset.num_sites
+        assert chi == dataset.chi
+        assert deltat == dataset.deltat
+        assert true_parameters == dataset.true_parameters
+        assert local_dim == dataset.local_dim
+        assert steps == dataset.steps
+        assert ini_state == dataset.ini_state
+
+    for s, k in zip(steps, keys):
+        time_stamp = deltat * s + times[-1] if times else deltat * s
+        times.append(time_stamp)
+
+        if dataset_containing_mps is not None:
+            m, errors_squared = next(mps_iterator), next(errors_iterator)
+        else:
+            t = time()
+            m, errors_squared = mps_evolution_order2(true_parameters, deltat, s, m)
+            print(f'Time evolution finished in {time() - t:.3f}s')
+
+        t = time()
+        samples = sample_from_mps(m, k)
+        print(f'Sampling finished in {time() - t:.3f}s')
+
+        samples_list.append(samples)
+        mps_list.append(m)
+        errors_squared_list.append(errors_squared)
+
+    dataset = DataSet(
+        num_sites=num_sites,
+        true_parameters=true_parameters,
+        ini_state=ini_state,
+        times=times,
+        samples_list=samples_list,
+        mps_list=mps_list,
+        errors_squared_list=errors_squared_list,
+        chi=chi,
+        deltat=deltat,
+        local_dim=local_dim,
+        prng_seed=seed,
+        run_manager_commit_hash=COMMIT_HASH,
+        differentiable_tebd_commit_hash=get_commit_hash(DIFFERENTIABLE_TEBD_DIR),
+        debug_mode=os.environ.get('DEBUG') == '1'
+    )
 
     now = datetime.now(UTC)
-    filename = f'{now:%y-%m-%d}-{name}-{ini_state_occupation}-n{cf.num_sites}.hdf5'
+    filename = f'{now:%y-%m-%d}-{name}-{ini_state}-n{num_sites}.hdf5'
     path = Path.joinpath(Path(DATASET_DIR), Path(filename))
-    with h5py.File(path, 'x') as f:
-        g_samples = f.create_group('samples')
-        g_mps = f.create_group('mps')
-        g_errs = f.create_group('errors_squared')
 
-        for i, (s, k) in enumerate(zip(steps, keys)):
-            time_stamp = cf.deltat * s + cf.times[-1] if cf.times else cf.deltat * s
-            cf.times.append(time_stamp)
-
-            t1 = time()
-            m, errors_squared = mps_evolution_order2(params, cf.deltat, s, m)
-            t2 = time()
-            evol_time = t2 - t1
-            print(f'Time evolution finished in {evol_time:.3f}s')
-            samples = sample_from_mps(m, k)
-            sample_time = time() - t2
-            print(f'Sampling finished in {sample_time:.3f}s')
-
-            g_samples.create_dataset(f't{i}', data=samples)
-            g_mps.create_dataset(f't{i}', data=m)
-            g_errs.create_dataset(f't{i}', data=errors_squared)
-
-        for k, v in cf.__dict__.items():
-            f.attrs[k] = v
-
-        f.attrs['prng_seed'] = seed
+    dataset.to_hdf5(path)
